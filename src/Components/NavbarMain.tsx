c16bb800d949c76e04810515e4c359099edf0cd8
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-08-14 23:50:55 +0700

    add dropdown

--- src/Components/NavbarMain.tsx
+++ src/Components/NavbarMain.tsx
@@ -10,26 +10,51 @@ export default function NavbarMain() {
                     </Link>
                     <div className="text-xl font-bold text-[#0CC0DF]">Addcition AI</div>
                 </div>
-                <ul className="flex items-center gap-[50px] w-fit">
+                <ul className="hidden sm:flex items-center gap-[50px] w-fit">
                     <li>
-                        <Link to="/" ><p 
-                        className="hover:font-bold hover:text-[#0CC0DF] hover:underline transition-all duration-300">Home</p></Link>
+                        <Link to="/" ><p
+                            className="hover:font-bold hover:text-[#0CC0DF] hover:underline transition-all duration-300">Home</p></Link>
                     </li>
                     <li>
                         {/* Pastikan Anda memiliki route untuk /gejala */}
-                        <Link to="/gejala" ><p 
-                        className="hover:font-bold hover:text-[#0CC0DF] hover:underline transition-all duration-300">Gejala</p></Link>
+                        <Link to="/gejala" ><p
+                            className="hover:font-bold hover:text-[#0CC0DF] hover:underline transition-all duration-300">Gejala</p></Link>
                     </li>
                     <li>
                         {/* Pastikan Anda memiliki route untuk /adiksi */}
-                        <Link to="/adiksi" ><p 
-                        className="hover:font-bold hover:text-[#0CC0DF] hover:underline transition-all duration-300">Adiksi</p></Link>
+                        <Link to="/adiksi" ><p
+                            className="hover:font-bold hover:text-[#0CC0DF] hover:underline transition-all duration-300">Adiksi</p></Link>
                     </li>
                 </ul>
+                {/* hiiden for mobile */}
+                <details className="dropdown dropdown-end sm:hidden">
+                    <summary className="btn m-1 rounded-full"></summary>
+                    <ul className="dropdown-content bg-[#0CC0DF] rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li className="p-2 mx-3 ">
+                            <Link to="/" ><p
+                                className="font-sans hover:font-bold hover:underline hover:text-white transition-all duration-300">Home</p></Link>
+                        </li>
+                        <li className="p-2 mx-3 ">
+                            {/* Pastikan Anda memiliki route untuk /gejala */}
+                            <Link to="/gejala" ><p
+                                className="font-sans hover:font-bold hover:underline hover:text-white transition-all duration-300">Gejala</p></Link>
+                        </li>
+                        <li className="p-2 mx-3 ">
+                            {/* Pastikan Anda memiliki route untuk /adiksi */}
+                            <Link to="/adiksi" ><p
+                                className="font-sans hover:font-bold hover:underline hover:text-white transition-all duration-300">Adiksi</p></Link>
+                        </li>
+                        <li className="p-2 mx-3 ">
+                            {/* Pastikan Anda memiliki route untuk /adiksi */}
+                            <Link to="/adiksi" ><p
+                                className="font-sans hover:font-bold hover:underline hover:text-white transition-all duration-300">Login Admin</p></Link>
+                        </li>
+                    </ul>
+                </details>
                 <a
                     href="#"
-                    className="flex items-center gap-2 rounded-full border border-[#0CC0DF] 
-                    py-3 px-5 font-semibold transition-all duration-300 hover:bg-[#0CC0DF]"
+                    className="hidden sm:flex items-center gap-2 rounded-full border border-[#0CC0DF] 
+                    py-2 px-4 m-3 font-semibold transition-all duration-300 hover:bg-[#0CC0DF]"
                 >
                     <p className=" text-[#0CC0DF] hover:text-white">Login Admin</p>
                 </a>
