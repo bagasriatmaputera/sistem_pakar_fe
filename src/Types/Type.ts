e109ba1f3d06ef955cd93e240b5fae0d300bc373
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-08-15 23:44:15 +0700

    update type.ts

--- src/Types/Type.ts
+++ src/Types/Type.ts
@@ -1,5 +1,18 @@
-export interface gejala {
+export interface Aturan {
+    id: number
+    gejala: Gejala,
+    adiksi: Adiksi,
+    cf_pakar: number
+}
+interface Gejala {
     id: number,
     kode: string,
     nama: string,
+}
+interface Adiksi {
+    id: number,
+    kode: string,
+    nama: string,
+    solusi: string,
+    deskripsi: string
 }
