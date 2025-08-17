6a1cfaf138758d3c2aa8e10a4f5aaa174d1f8406
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-08-17 22:27:33 +0700

    update types.tsx

--- src/Types/Type.ts
+++ src/Types/Type.ts
@@ -1,18 +1,27 @@
 export interface Aturan {
-    id: number
-    gejala: Gejala,
-    adiksi: Adiksi,
-    cf_pakar: number
+    id: number;
+    gejala: Gejala;
+    adiksi: Adiksi;
+    cf_pakar: number;
 }
+
+export interface Answer {
+    gejala_id: number;
+    adiksi_id: number;
+    cf_kombinasi: number;
+    cf_user: number;
+    cf_pakar: number;
+}
+
 interface Gejala {
-    id: number,
-    kode: string,
-    nama: string,
+    id: number;
+    kode: string;
+    nama: string;
 }
 interface Adiksi {
-    id: number,
-    kode: string,
-    nama: string,
-    solusi: string,
-    deskripsi: string
+    id: number;
+    kode: string;
+    nama: string;
+    solusi: string;
+    deskripsi: string;
 }
