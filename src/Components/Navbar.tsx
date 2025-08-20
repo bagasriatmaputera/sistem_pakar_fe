3e4c0c869ff9b32116ed8cd4e938840b5a33bd7e
Bagasbelajarhtml <145095794+Bagasbelajarhtml@users.noreply.github.com>
2025-08-21 00:15:02 +0700

    done

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -2,20 +2,20 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <nav className="bg-white shadow-xl">
-            <div className="flex items-center justify-between w-full max-w-[1130px] py-[22px] mx-auto">
+        <nav className="bg-white shadow-xl font-sans">
+            <div className="flex items-center justify-between w-full py-[22px] mx-auto">
                 <div className="flex justify-center items-center">
                     <Link to={'/'}>
                         <img width={55} src="/public/assets/AI Check logo.png" alt="logo" />
                     </Link>
-                    <div className="text-xl font-bold text-[#0CC0DF]">Addcition AI</div>
+                    <div className="text-md md:text-xl  font-bold text-[#0CC0DF]">Addcition AI</div>
                 </div>
                 <a
                     href="http://127.0.0.1:8000/admin"
                     className="flex items-center gap-2 rounded-full border border-[#0CC0DF] 
                     py-3 px-5 font-semibold transition-all duration-300 hover:bg-[#0CC0DF]"
                 >
-                    <p className=" text-[#0CC0DF] hover:text-white">Login Admin</p>
+                    <p className="text-md md:text-xl text-[#0CC0DF] hover:text-white">Login Admin</p>
                 </a>
 
             </div>
